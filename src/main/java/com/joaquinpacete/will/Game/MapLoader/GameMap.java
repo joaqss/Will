f72ed61042b7648af6e088b15dcee3899659a9ab
Joaquin Pacete <89520265+joaqss@users.noreply.github.com>
2024-07-17 18:33:44 +0800

    Fixed data import and Display

--- src/main/java/com/joaquinpacete/will/Game/MapLoader/GameMap.java
+++ src/main/java/com/joaquinpacete/will/Game/MapLoader/GameMap.java
@@ -1,5 +1,7 @@
 package com.joaquinpacete.will.Game.MapLoader;
 
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
@@ -9,17 +11,23 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.*;
-import java.net.URL;
 import java.util.Arrays;
 
 public class GameMap {
+    int mapWidth;
+    int mapHeight;
+    int tileWidth;
+    int tileHeight;
+    int[] tileNumbers;
 
     public GameMap() throws IOException {
         InputStream filepath = getClass().getResourceAsStream("/Game/Maps/map1.xml");
-        loadMap(filepath);
+        importMap(filepath);
     }
 
-    private void loadMap(InputStream filepath)  {
+    private void importMap(InputStream filepath)  {
+
+
         try {
             // DOM parser, suitable for xml files
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance(); // return a new instance of a DocumentBuilderFactory
@@ -27,17 +35,31 @@ public class GameMap {
             Document doc = builder.parse(filepath); // return an instance ng document
             NodeList mapData = doc.getElementsByTagName("data"); // will check a tag na may "data" sa xml file
 
+            mapWidth = Integer.parseInt(doc.getDocumentElement().getAttribute("width"));
+            mapHeight = Integer.parseInt(doc.getDocumentElement().getAttribute("height"));
+            tileWidth = Integer.parseInt(doc.getDocumentElement().getAttribute("tilewidth"));
+            tileHeight = Integer.parseInt(doc.getDocumentElement().getAttribute("tileheight"));
+
             if (mapData.getLength() > 0) {
                 Element dataElement = (Element) mapData.item(0); // get the first element
                 // remove spaces and new lines and get the text content
                 String csvData = dataElement.getTextContent().trim().replaceAll("[\\n\\r\\s]+", "");
                 String[] tiles = csvData.split(","); // remove comma and split the string
-                int[] tileNumbers = Arrays.stream(tiles).mapToInt(Integer::parseInt).toArray(); // convert to int
+                tileNumbers = Arrays.stream(tiles).mapToInt(Integer::parseInt).toArray(); // convert to int
+
+                System.out.println("Map Width: " + mapWidth);
+                System.out.println("Map Height: " + mapHeight);
+                System.out.println("Tile Width: " + tileWidth);
+                System.out.println("Tile Height: " + tileHeight);
 
                 // print tileNumbers
-                for (int i = 0; i < tileNumbers.length; i++) {
-                    System.out.println(tileNumbers[i]);
+                for (int i = 0; i < mapHeight; i++) {
+                    for (int j = 0; j < mapWidth; j++) {
+                        System.out.print(tileNumbers[i + j] + " ");
+                    }
+                    System.out.println();
                 }
+
             }
         } catch (ParserConfigurationException e) {
             System.err.println("Parser Configuration Error: " + e.getMessage());
@@ -51,5 +73,12 @@ public class GameMap {
             System.err.println("General Exception: " + e.getMessage());
             e.printStackTrace();
         }
+    }
+
+
+    public void drawMap(GraphicsContext gc) {
+        InputStream tilesetPath = getClass().getResourceAsStream("/Game/Images/grass.png");
+        Image tileset = new Image(tilesetPath, tileWidth, tileHeight, false, false);
+        gc.drawImage(tileset, 0, 0, tileWidth, tileHeight, 0, 0, tileWidth, tileHeight);
     }
 }
--- src/main/java/com/joaquinpacete/will/Game/RunTime/Display.java
+++ src/main/java/com/joaquinpacete/will/Game/RunTime/Display.java
@@ -5,6 +5,8 @@ import com.joaquinpacete.will.Game.MapLoader.GameMap;
 import com.joaquinpacete.will.Game.Object.Player;
 import com.joaquinpacete.will.Game.Overlays.Hotbar;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -19,19 +21,30 @@ public class Display extends Engine {
     }
 
     public void setupAndShowStage(Stage stage) throws IOException {
+        // default settings
         Pane root = new Pane();
         root.setPrefSize(getWidth(), getHeight());
-        System.out.println(root.getWidth());
+        GameLoop gameLoop = new GameLoop(this);
 
+        // canvas and drawing initialization
+        Canvas canvas = new Canvas(getWidth(), getHeight());
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        GameMap gameMap = new GameMap();
+        gameMap.drawMap(gc);
+        root.getChildren().add(canvas);
+
+        // player initialization
         player = new Player(500,500,50,50);
         root.getChildren().add(player.getGraphic()); // Add player to the pane
 
+        // hotbar initialization
         Hotbar hotbar = new Hotbar(this, root);
         scene = new Scene(root, getWidth(), getHeight());
-        GameLoop gameLoop = new GameLoop(this);
-        Controls controls = new Controls(this, gameLoop, hotbar);
-        GameMap gameMap = new GameMap();
 
+        // controls initialization
+        Controls controls = new Controls(this, gameLoop, hotbar);
+
+        // game loop initialization
         Thread gameLoopThread = new Thread(gameLoop);
         gameLoopThread.start();
 
